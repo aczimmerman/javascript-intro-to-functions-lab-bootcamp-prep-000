d0de87966c76bf0d46117605355ec83d76357fac
Andrea Zimmerman <andrea.c.zimmerman@gmail.com>
2017-09-20 21:50:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,9 +17,12 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === "I love you, Grandma.") {
     return "I love you, too."
-  } else if (string.toLowerCase === string) {
+  } 
+  if (string.toLowerCase === string) {
     return "I can't hear you!"
-  } else (string.toUpperCase === string){
+  } 
+  if (string.toUpperCase === string){
     return "YES INDEED!"
   }
+  return "Hello Dear"
 }
